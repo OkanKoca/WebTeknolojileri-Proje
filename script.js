5d35274d27832d215eb0d92e799df838fec015ff
Okan Koca <okankoca67@gmail.com>
2024-05-13 19:18:48 +0300

    slider foto, navbar linkler, api denemeleri

---
+++ script.js
@@ -0,0 +1,29 @@
+//TMDB API
+
+const API_KEY = 'api_key=ddceaaf80e8a6017f8d8a30c7e8c2460';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
+
+GetMovies(API_URL);
+
+function GetMovies(url)
+{
+    fetch(url)
+        .then(res => res.json()) 
+        .then(data => {
+            console.log(data);
+            showMovies(data.results);
+        })
+        .catch(error => {
+            console.error('Error fetching movies:', error);
+        });
+}
+
+function showMovies(data)
+{
+    data.forEach(movie => {
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = ''
+    })
+}
